2d1f336709def62eb14b2f138ea3188ef981dac8
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2021-11-10 13:40:27 +0100

    esercizio-dadi

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-let listaEmail = ["cane.gatto@gmail.com", "esercito.jim@libero.it", "bue.o@yahoo.it"]
+// let listaEmail = ["cane.gatto@gmail.com", "esercito.jim@libero.it", "bue.o@yahoo.it"];
 
-let input = prompt("inserisci email");
+// let input = prompt("inserisci email");
 
-let index = 0;
-for (i = 0; i < listaEmail.length; i++) {
-    if (input == listaEmail[i]) {
-        index = 1
-    }    
-}
+// let index = 0;
+// for (i = 0; i < listaEmail.length; i++) {
+//     if (input == listaEmail[i]) {
+//         index = 1
+//     }    
+// }
 
-if (index == 1) {
-    console.log("Benvenuto")
+// if (index == 1) {
+//     console.log("Benvenuto")
+// }
+// else {
+//     console.log("indirizzo email errato")
+// }
+
+
+// Esercizio Dadi 
+
+
+console.log("giocatore 1")
+let g1 = Math.floor(Math.random() * (6 - 1)+1)
+console.log(g1);
+
+
+console.log("giocatore 2")
+let g2 = Math.floor(Math.random() * (6 - 1)+1)
+console.log(g2);
+
+if (g1 > g2) {
+    console.log("Giocatore 1 vince")
 }
 else {
-    console.log("indirizzo email errato")
+    console.log("Giocatore 2 vince")
 }
